4eeeff1e08e3931e411b30cc4df7fbeae2762761
atl043 <andrewfourli@gmail.com>
2019-04-24 21:46:21 -0700

    initial upload

---
+++ milestone2/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+// Learn more: http://expressjs.com/en/starter/static-files.html
+// To learn more about server routing:
+// Express - Hello world: http://expressjs.com/en/starter/hello-world.html
+// Express - basic routing: http://expressjs.com/en/starter/basic-routing.html
+// Express - routing: https://expressjs.com/en/guide/routing.html
+app.use(express.static('static_files'));
+
+// simulates a database in memory, to make this example simple and
+// self-contained (so that you don't need to set up a separate database).
+// note that a real database will save its data to the hard drive so
+// that they become persistent, but this fake database will be reset when
+// this script restarts. however, as long as the script is running, this
+// database can be modified at will.
+const colorDatabase = {
+  'black': { colors: ["#D6DFE0", "#07DAC0", "#CE2380"], pic: 'black.png' },
+  'pink': { colors: ["#36EEE0", "#BCECE0", "#4C5270"], pic: 'pink.png' },
+  'green': { colors: ["#005E38", "#C98D26", "#DEDCE4"], pic: 'green.png' }
+};
+// GET a list of all usernames
+//
+// To test, open this URL in your browser:
+//   http://localhost:3000/users
+app.get('/colorCombo', (req, res) => {
+  const allColorCombo = Object.keys(colorDatabase); // returns a list of object keys
+  console.log('allColorCombo is:', allColorCombo);
+  res.send(allColorCombo);
+});
+
+
+// GET profile data for a user
+//
+// To test, open these URLs in your browser:
+//   http://localhost:3000/users/Philip
+//   http://localhost:3000/users/Carol
+//   http://localhost:3000/users/invalidusername
+app.get('/colorCombo/:color', (req, res) => {
+  const nameToLookup = req.params.color; // matches ':color' above
+  const val = colorDatabase[nameToLookup];
+  console.log(nameToLookup, '->', val); // for debugging
+  if (val) {
+    res.send(val);
+  } else {
+    res.send({}); // failed, so return an empty object instead of undefined
+  }
+});
+
+// start the server at URL: http://localhost:3000/
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000/');
+});
