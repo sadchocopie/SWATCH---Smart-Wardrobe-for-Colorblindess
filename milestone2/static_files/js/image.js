9ce80ba07931d03d52a3abc9a80e87b7276431b0
atl043 <andrewfourli@gmail.com>
2019-04-28 17:24:43 -0700

    added test.html for upload image get color

--- milestone2/static_files/js/image.js
+++ milestone2/static_files/js/image.js
@@ -20,8 +20,95 @@ $(function () {
             reader.onloadend = function () { // set image data as background of div
                 //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
                 uploadFile.closest(".imgUpload").find('.imagePreview').css("background-image", "url(" + this.result + ")");
+                console.log("test")
+                handleFiles(this.result);
             }
         }
 
     });
+});
+const colorThief = new ColorThief();
+
+// Run Color Thief functions and display results below image.
+// We also log execution time of functions for display.
+const showColorsForImage = function ($image, $imageSection) {
+    let image = $image[0];
+    let start = Date.now();
+    let color = colorThief.getColor(image);
+    let elapsedTimeForGetColor = Date.now() - start;
+    let palette = colorThief.getPalette(image);
+    let elapsedTimeForGetPalette = Date.now() - start + elapsedTimeForGetColor;
+
+    let colorThiefOutput = {
+        color: color,
+        palette: palette,
+        elapsedTimeForGetColor: elapsedTimeForGetColor,
+        elapsedTimeForGetPalette: elapsedTimeForGetPalette
+    };
+    console.log(colorThiefOutput);
+
+    let colorThiefOuputHTML = Mustache.to_html($('#color-thief-output-template').html(), colorThiefOutput);
+
+    $imageSection.addClass('with-color-thief-output');
+    $imageSection.find('.run-functions-button').addClass('hide');
+
+    setTimeout(function () {
+        $imageSection.find('.color-thief-output').append(colorThiefOuputHTML).slideDown();
+        // If the color-thief-output div is not in the viewport or cut off, scroll down.
+        var windowHeight = $(window).height();
+        var currentScrollPosition = $('body').scrollTop()
+        var outputOffsetTop = $imageSection.find('.color-thief-output').offset().top
+        if ((currentScrollPosition < outputOffsetTop) && (currentScrollPosition + windowHeight - 250 < outputOffsetTop)) {
+            $('body').animate({ scrollTop: outputOffsetTop - windowHeight + 200 + "px" });
+        }
+    }, 300);
+};
+function handleFiles(files) {
+    var $draggedImages = $('#dragged-images');
+    var imageType = /image.*/;
+    var fileCount = files.length;
+
+    for (var i = 0; i < fileCount; i++) {
+        var file = files[i];
+
+        if (file.type.match(imageType)) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                imageInfo = {
+                    images: [
+                        { 'class': 'dropped-image', file: event.target.result }
+                    ]
+                };
+
+                var imageSectionHTML = Mustache.to_html($('#image-section-template').html(), imageInfo);
+                $draggedImages.prepend(imageSectionHTML);
+
+                var $imageSection = $draggedImages.find('.image-section').first();
+                var $image = $('.dropped-image .target-image');
+
+                // Must wait for image to load in DOM, not just load from FileReader
+                $image.on('load', function () {
+                    showColorsForImage($image, $imageSection);
+                });
+            };
+            reader.readAsDataURL(file);
+        } else {
+            alert('File must be a supported image type.');
+        }
+    }
+}
+
+
+$(document).ready(function () {
+    // Event handlers
+    $('.run-functions-button').on('click', function (event) {
+        let $this = $(this);
+        $this.text('...');
+        let $imageSection = $this.closest('.image-section');
+        let $colorThiefOutput = $imageSection.find('.color-thief-output');
+        let $targetimage = $imageSection.find('.target-image');
+        showColorsForImage($targetimage, $imageSection);
+    });
+
+
 });
---
+++ milestone2/static_files/js/test.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            imageSrc: null,
+            swatches: []
+        }
+    },
+    methods: {
+        gotPic(event) {
+            this.imageSrc = URL.createObjectURL(event.target.files[0]);
+        },
+        getSwatches() {
+            console.log("test")
+            let colorThief = new ColorThief();
+            let colorArr = colorThief.getPalette(this.$refs.theImage, 2);
+            // let colorArr = colorThief.getColor(this.$refs.theImage);
+
+            //reset
+            this.swatches = [];
+            colorArr.forEach(c => {
+                let style = {
+                    backgroundColor: "rgb(" + c[0] + "," + c[1] + "," + c[2] + ")"
+                }
+                this.swatches.push({ style });
+            });
+
+            // let style = {
+            //     backgroundColor: "rgb(" + colorArr[0] + "," + colorArr[1] + "," + colorArr[2] + ")"
+            // }
+            // this.swatches.push({ style });
+        }
+    }
+});
+
