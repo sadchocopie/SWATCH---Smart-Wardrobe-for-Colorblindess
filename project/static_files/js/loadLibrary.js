8bb729e39bd08b81da9b35c8899c218d0c051c0b
atl043 <andrewfourli@gmail.com>
2019-05-09 18:35:26 -0700

    library loads from firebase

--- project/static_files/js/loadLibrary.js
+++ project/static_files/js/loadLibrary.js
@@ -6,22 +6,44 @@ Vue.component('obj', {
     template: '<div class="itemBox"><p> {{ item }} </p><img class="imgBox" v-bind:src="image" style="width:90%; margin:auto;" />       </div>'
 });
 
+const database = firebase.database();
+let clothesList = [];
+database.ref('Posts/').on('value', (snapshot) => {
+    const allClothes = snapshot.val();
+    console.log('Posts/ changed:', allClothes);
+    clothesList.length = 0
+    if (allClothes) {
+        Object.keys(allClothes).forEach((key) => {
+            clothesList.push(
+                {
+                    type: allClothes[key].type,
+                    selected: false,
+                    imgSrc: allClothes[key].url
+                }
+            );
+        });
+    }
 
+});
 const app = new Vue({
     el: '#app',
     data: {
         message: 'Please select 2 items (do green shirt and gray pants for now):',
-        objectTypes: [
+        objectTypes: clothesList,
+
+        /*
+        [
             { type: 'Green shirt', selected: false, imgSrc: '../images/shirt.jpg' },
             { type: 'White shirt', selected: false, imgSrc: '../images/whiteTee.jpg' },
             { type: 'gray pants', selected: false, imgSrc: '../images/pants.jpg' },
             { type: 'Pink Hooide', selected: false, imgSrc: '../images/pinkHoodie.jpg' }
-        ],
-        
-//        imageSources: [
-//            { imgSrc: '../images/shirt.jpg'},
-//            { imgSrc: '../images/pants.jpg'}
-//        ]
+        ]
+        */
+
+        //        imageSources: [
+        //            { imgSrc: '../images/shirt.jpg'},
+        //            { imgSrc: '../images/pants.jpg'}
+        //        ]
     },
     methods: {
         selectObjectType: function (object) {
